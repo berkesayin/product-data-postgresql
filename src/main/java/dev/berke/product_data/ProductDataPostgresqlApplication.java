c0767d1fe1d6512bbe704c06d8426b7631c61f03
Berke Sayın <sayinberke34@gmail.com>
2025-04-30 19:40:55 +0300

    first commit

---
+++ src/main/java/dev/berke/product_data/ProductDataPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package dev.berke.product_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductDataPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductDataPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/berke/product_data/ProductDataPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.berke.product_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDataPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
