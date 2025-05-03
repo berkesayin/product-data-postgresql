de2432eda9fba74e9e3d617eacbcbe0160f35ee3
Berke Sayın <sayinberke34@gmail.com>
2025-05-03 16:27:00 +0300

    Update configurations

--- src/main/java/dev/berke/product_data/ProductApplication.java
+++ src/main/java/dev/berke/product_data/ProductApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductDataPostgresqlApplication {
+public class ProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductDataPostgresqlApplication.class, args);
+		SpringApplication.run(ProductApplication.class, args);
 	}
 
 }
